f6e7a701165cef7773de68828f996b1361fbbd3d
Adair Duarte <ada_dp10@hotmail.com>
2023-09-15 11:57:42 -0600

    ConexionApi
    Se hizo la conexion con google Maps de manera exitosa

--- app/src/main/java/com/example/maps/MainActivity.kt
+++ app/src/main/java/com/example/maps/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.maps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var mMap: GoogleMap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //Obtencion del mapa de XML (Fragment)
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+        mMap = p0
+
+        // Añadir un marcador en una ubicación
+        val ubicacion = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(ubicacion).title("Marcador en Sidney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ubicacion))
     }
 }
